ff0dbca15c4cf20011ffd2b22e873f0c9d009d75
Suneel Shivani <163507701+suneelshivanioffical@users.noreply.github.com>
2026-07-29 17:44:22 +0500

    Add files via upload

---
+++ Shipping_Cost_Predication/README.md
@@ -0,0 +1,107 @@
+# Shipping Cost Prediction using Machine Learning
+
+Businesses involved in trade often need to estimate shipping costs before a shipment is dispatched. Accurate cost prediction helps freight forwarders, logistics providers, exporters, and importers improve budgeting, pricing strategies, and operational planning. 
+
+This model uses a Gradient Boosting Regressor to predict shipping costs based on key shipment information such as trade direction, cargo type, ports, Incoterms, vessel type, cargo volume, and transit days
+
+The model trained on historical maritime trade data, is deployed as an interactive Streamlit web application.
+
+## Demo
+
+Link: 
+![]()
+
+## Dataset
+
+Pakistan Maritime Trade Dataset (2020–2026)
+
+```
+https://www.kaggle.com/datasets/hammadansari7/pakistan-maritime-trade-and-shipping-dataset
+```
+
+## Project Structure
+
+```
+SHIPPING_COST_PREDICTION
+│
+├── data
+│   └── pakistan_maritime_trade_2020_2026.csv    # Dataset
+│
+├── models
+│   ├── ShippingCostPredication.pkl              # Trained ml model
+│   └── encoder.pkl                              # Label encoder for categorical features
+│
+├── notebook
+│   └── Shipping Cost Predication.ipynb          # Model Training
+│
+├── app.py                                       # Streamlit UI
+├── style.css                                    # Custom UI styling
+├── requirements.txt                             # Dependencies
+└── README.md                                    # Project documentation
+```
+
+## Technologies
+
+- Python
+- Pandas
+- NumPy
+- Scikit-learn
+- Streamlit UI
+- Pickle
+
+## Model
+
+```
+Gradient Boosting Regressor
+```
+
+## Model Performance
+
+
+The machine learning model was tested to see how well it can predict shipping cost.
+
+Results:
+
+- R² Score: 74.2%
+- Average Prediction Error (MAE): 32.85
+- Root Mean Squared Error (RMSE): 39.47
+
+What this means (Simple Explanation)
+
+- On average, the predicted shipping cost differs from the actual cost by about 33 units.
+- The model explains approximately 74% of the factors affecting shipping costs.
+- Overall, it provides reliable estimates for shipping cost prediction.
+
+---
+
+## Learning Outcome
+
+This project demonstrates practical experience in:
+
+- Machine Learning Regression
+- Data Preprocessing
+- Feature Engineering
+- Label Encoding
+- Model Evaluation
+- Streamlit Application Development
+- Deployment on Streamlit Cloud for Portfilo
+- FastAPI REST API Development
+- Frontend & Backend Integration
+- Docker Containerization
+- Azure Cloud Deployment
+- Production-ready ML Application
+
+---
+
+## Further Work Done
+
+Beyond the direct deployment on Streamlit Cloud for Portfolio, the following work was also completed:
+
+- Developed a FastAPI backend to serve the ML model.
+- Containerized the backend using Docker and push to the Azure Container App Services.
+- Deployed the Dockerized application to Azure Web Services.
+- Removed Azure resources after deployment to avoid unnecessary cloud costs.
+
+---
+
+**Built with ❤️ using Python, FastAPI, Scikit-learn, Gradient Boosting Regressor, Streamlit, Docker and Azure Cloud.**
---
+++ Shipping_Cost_Predication/app.py
@@ -0,0 +1,131 @@
+import os
+import pickle
+import pandas as pd
+import streamlit as st
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+model_path = os.path.join(
+    BASE_DIR,
+    "models",
+    "ShippingCostPredication.pkl"
+)
+
+encoder_path = os.path.join(
+    BASE_DIR,
+    "models",
+    "encoder.pkl"
+)
+
+
+model = pickle.load(open(model_path, "rb"))
+encoder = pickle.load(open(encoder_path, "rb"))
+
+# Load model
+model = pickle.load(open("models/ShippingCostPredication.pkl", "rb"))
+encoder = pickle.load(open("models/encoder.pkl", "rb"))
+
+# Page configuration
+st.set_page_config(page_title="Shipping Cost Prediction",page_icon="🚢",layout="centered")
+
+st.title("🚢 Shipping Cost Prediction")
+st.write("Enter shipment details to predict shipping cost")
+
+css_path = os.path.join(BASE_DIR, "style.css")
+
+with open(css_path, encoding="utf-8") as f:
+    css = f.read()
+
+st.markdown(
+    f"<style>{css}</style>",
+    unsafe_allow_html=True
+)
+
+col1, col2 = st.columns(2)
+
+with col1:
+
+# Input fields
+
+    trade_direction = st.selectbox("Trade Direction", ["Export", "Import"])
+
+    cargo_type = st.selectbox("Cargo Type", ["Electronics", "Crude Oil", "Machinery", "Steel Coils", "Rice", "Wheat", "Textiles", "Fuel Oil"])
+
+    incoterms = st.selectbox("Incoterms",["EXW","CFR","DDP","FOB","FAS","CIF"])
+
+    port_loading = st.selectbox("Port of Loading",["Karachi Port","Port Qasim, Karachi","Gwadar Port","Singapore","Rotterdam, Netherlands","Hamburg, Germany"])
+
+with col2:
+
+    port_discharge = st.selectbox("Port of Discharge",["Singapore","Karachi Port","Port Qasim, Karachi","Rotterdam, Netherlands","Hamburg, Germany"])
+
+    vessel_type = st.selectbox("Vessel Type",["Container Ship","Bulk Carrier (Handymax)","General Cargo Ship","VLCC","MR Tanker"])
+
+    cargo_volume = st.number_input("Cargo Volume (MT)",min_value=2016.85,value=299985.15)
+
+    transit_days = st.number_input("Transit Days",min_value=5.0,value=35.0)
+
+
+# -----------------------------
+# Predict Button + Model Cards
+# -----------------------------
+
+col_btn, col_acc, col_model, col_features = st.columns([3,1,1,1])
+
+
+with col_btn:
+    predict = st.button("Predict")
+
+
+with col_acc:
+    st.markdown("""
+    <div class="mini-card">
+        <div class="card-title">74.2%</div>
+        <div class="card-subtitle">Model Accuracy</div>
+    </div>
+    """, unsafe_allow_html=True)
+
+
+with col_model:
+    st.markdown("""
+    <div class="mini-card">
+        <div class="card-title">GBoostingRegressor</div>
+        <div class="card-subtitle">Training Algorithm</div>
+    </div>
+    """, unsafe_allow_html=True)
+
+
+with col_features:
+    st.markdown("""
+    <div class="mini-card">
+        <div class="card-title">8</div>
+        <div class="card-subtitle">Features</div>
+    </div>
+    """, unsafe_allow_html=True)
+
+if predict:
+
+    categorical = pd.DataFrame([{
+        "Trade_Direction": trade_direction,
+        "Cargo_Type": cargo_type,
+        "Incoterms": incoterms,
+        "Port_of_Loading": port_loading,
+        "Port_of_Discharge": port_discharge,
+        "Vessel_Type": vessel_type
+    }])
+
+    encoded = encoder.transform(categorical).toarray()
+
+    numerical = [
+        cargo_volume,
+        transit_days
+    ]
+
+    features = [numerical + encoded[0].tolist()]
+
+    prediction = model.predict(features)
+
+    st.success(
+        f"Estimated Shipping Cost: ${prediction[0]:,.2f}"
+    )
